f85fd321fcbcb01153b448eabf245edd2599a937
Saurabh Taware <saurabh.taware@tuni.fi>
2020-10-05 13:14:11 +0300

    added indentation

--- countries-ui/src/components/CountryTableRow/CountryTableRow.js
+++ countries-ui/src/components/CountryTableRow/CountryTableRow.js
@@ -4,23 +4,22 @@ import { Button } from "react-bootstrap";
 
 
 
-export default function CountryTableRow({countryName,countryCode,onCountryLinkClick}) {
+export default function CountryTableRow({ countryName, countryCode, onCountryLinkClick }) {
 
-    
 
-    const handleClick = (event) =>{
+
+    const handleClick = (event) => {
         onCountryLinkClick(event.target.value)
     }
 
 
-    return(
-        <>
+    return (
+
         <tr>
-        <td>
-        <Button variant="link" value={countryName} onClick={handleClick}>{countryName}</Button></td>
-        <td>{countryCode}</td>
+            <td>
+                <Button variant="link" value={countryName} onClick={handleClick}>{countryName}</Button></td>
+            <td>{countryCode}</td>
         </tr>
-        </>
 
 
     )
